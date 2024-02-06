e05f5c246cde4dfc28453fb1b6ba9c5fc3d8d842
XesIhma <kamil_starzyk@outlook.com>
2024-02-06 22:08:46 +0100

    time editor somehow finished

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
   <div id="app">
     <div v-if="time">
       <TimeEditor :time="time" @change="(time) => Object.assign(this.time, time)"/>
-    </div>
+      {{ JSON.stringify(time) }}
+      </div>
     <div v-else>
       <p>Loading...</p>
     </div>
     <!-- Other components using specific data -->
+
+    <br>
+    <button @click="downloadJSON">Zrzuć JSONa</button>
   </div>
 </template>
 
@@ -36,6 +40,29 @@ export default {
       .catch(error => {
         console.error('Error fetching data:', error);
       });
+  },
+  methods: {
+    downloadJSON() {
+      // eslint-disable-next-line no-unused-vars
+      const {isTrusted, _vts, ...rest} = this.time
+      const jsonData = JSON.stringify({...this.gameData, time:rest});
+
+      // Create a Blob object to store the JSON data
+      const blob = new Blob([jsonData], { type: "application/json" });
+
+      // Create a temporary anchor element
+      const link = document.createElement("a");
+      link.href = window.URL.createObjectURL(blob);
+
+      // Set the filename for the downloaded JSON file
+      link.download = "game_data.json";
+
+      // Trigger the click event to download the JSON file
+      link.click();
+
+      // Cleanup
+      window.URL.revokeObjectURL(link.href);
+    }
   }
 };
 </script>
--- src/components/TimeEditor.vue
+++ src/components/TimeEditor.vue
@@ -2,14 +2,38 @@
   <div>
     <h1>Time Details</h1>
     <div>
-      <p>Seconds: {{ time.s }}</p>
-      <p>Minutes: {{ time.m }}</p>
-      <p>Hours: {{ time.h }}</p>
-
+      <p>Seconds: <input
+        :value="time.s" 
+        @input=" ( event ) => handleUpdate(event, 's')"
+      ></p>
+      <p>Minutes: <input
+        :value="time.m" 
+        @input=" ( event ) => handleUpdate(event, 'm')"
+      ></p>
       <p>Hours: <input
         :value="time.h" 
-        @input="handleUpdate"></p>
-        <!-- Display other time properties -->
+        @input=" ( event ) => handleUpdate(event, 'h')"
+      ></p>
+      <p>Days: <input
+        :value="time.d" 
+        @input=" ( event ) => handleUpdate(event, 'd')"
+      ></p>
+      <p>Week Day: <input
+        :value="time.week_d" 
+        @input=" ( event ) => handleUpdate(event, 'week_d')"
+      ></p>
+      <p>Week: <input
+        :value="time.w" 
+        @input=" ( event ) => handleUpdate(event, 'w')"
+      ></p>
+      <p>Season: <input
+        :value="time.sn" 
+        @input=" ( event ) => handleUpdate(event, 'sn')"
+      ></p>
+      <p>Years: <input
+        :value="time.y" 
+        @input=" ( event ) => handleUpdate(event, 'y')"
+      ></p>
 
     </div>
   </div>
@@ -21,8 +45,8 @@ export default {
     time: Object // Define time as a prop
   },
   methods: {
-    handleUpdate(event){
-      const objekt = { ...this.time, h:event.target.value }
+    handleUpdate(event, key){
+      const objekt = { ...this.time, [key]:event.target.value }
       this.$emit('change', objekt)
       //console.log(objekt)
       console.log(this.time)
