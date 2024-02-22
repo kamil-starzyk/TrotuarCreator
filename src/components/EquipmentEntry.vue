b30ef3bb31df9f6551db0889066ca9463c9b6ff3
XesIhma <kamil_starzyk@outlook.com>
2024-02-22 23:36:16 +0100

    changed card into bsmodal

--- src/components/EquipmentEntry.vue
+++ src/components/EquipmentEntry.vue
@@ -1,49 +1,75 @@
 <template>
-  <button class="btn btn-primary" @click="cardVisible = !cardVisible">
+  <button class="btn btn-primary" @click="openModal">
     {{item.name || ">Pusty slot<"}}
   </button>
-  <div class="card item_card" style="width: 18rem" v-show="cardVisible">
-    <div class="card-body">
-      <h5 class="card-title">{{ item.name }}</h5>
-      <button
-        class="btn btn-close"
-        @click="cardVisible = !cardVisible"
-      ></button>
-      <p class="card-text">{{ item.description }}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-      <input
-        :value="item.name"
-        @input="(event) => handleItemUpdate(event, 'name')"
-      />
-      <textarea
-        :value="item.description"
-        @input="(event) => handleItemUpdate(event, 'description')"
-      ></textarea>
-      <input
-        :value="item.weight"
-        @input="(event) => handleItemUpdate(event, 'weight', true)"
-        class="numeric_input"
-      />
+
+
+  <div class="modal fade" ref="itemModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="recipient-name" class="col-form-label">Recipient:</label>
+              <input
+                :value="item.name"
+                @input="(event) => handleItemUpdate(event, 'name')"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="message-text" class="col-form-label">Message:</label>
+              <textarea
+                :value="item.description"
+                @input="(event) => handleItemUpdate(event, 'description')"
+              ></textarea>
+            </div>
+            <div class="mb-3">
+              <label for="recipient-name" class="col-form-label">TODO label weight:</label>
+              <input
+                :value="item.weight"
+                @input="(event) => handleItemUpdate(event, 'weight', true)"
+                class="numeric_input"
+              />
+            </div>
+
+            <div class="mb-3">
+              <GenericArray
+                :items="Object.entries(item.attr).map( ([key, value]) => ({key, value}))"
+                :new-item="{ name: 'nazwa', description: 'desc', weight: 1 }"
+                :renderComponent="renderComponentEquipment"
+                @change="editItemAttr"
+              >
+              </GenericArray>
+            </div>
+
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Send message</button>
+        </div>
+      </div>
     </div>
   </div>
+
 </template>
 
 <style>
-.item_card {
-  position: fixed;
-  top: 100px;
-  left: 200px;
-  box-shadow: 0px 0px 15px 1000000px rgba(0, 0, 0, 0.5);
-}
 </style>
 
 <script>
+import GenericArray from "./GenericArray.vue";
+//TODO generic dictionary
 import { toRaw } from "vue";
 import _ from "lodash";
 export default {
   data() {
     return {
-      cardVisible: false,
+      modal: null
     };
   },
   props: {
@@ -57,9 +83,20 @@ export default {
 
       this.$emit("change", record);
     },
+    openModal(){
+      this.modal.show()
+    },
+    editItemAttr(value){
+      //TODO
+      value.map()
+      this.handleUpdate({ target: { value } }, 'equipment')
+    }
   },
   mounted() {
-    console.log(this.item);
+    this.modal = new window.bootstrap.Modal(this.$refs.itemModal)
   },
+  unmounted() {
+    this.modal.dispose()
+  }
 };
 </script>
